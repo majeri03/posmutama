6a369268ae8c627528cb7ea8dd6b53587aad8a3a
Majeri <139302074+majeri03@users.noreply.github.com>
2025-06-09 08:56:16 +0000

    bismillahh

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,17 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-fun localProperties(project: org.gradle.api.Project): java.util.Properties {
-    val properties = java.util.Properties()
+fun localProperties(project: org.gradle.api.Project): Properties {
+    val properties = Properties()
     val localPropertiesFile = project.rootProject.file("local.properties")
     if (localPropertiesFile.exists()) {
-        properties.load(java.io.FileInputStream(localPropertiesFile))
+        properties.load(FileInputStream(localPropertiesFile))
     }
     return properties
 }
